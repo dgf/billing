71483891a4481d312d81e05dc7081a6e5bf7e533
andre <andrzimmermann@gmx.de>
2018-08-04 00:44:41 +0100

    create table view and bind invoice list

--- desktop/src/main/java/org/aplatanao/billing/desktop/Controller.java
+++ desktop/src/main/java/org/aplatanao/billing/desktop/Controller.java
@@ -1,21 +1,56 @@
 package org.aplatanao.billing.desktop;
 
+import javafx.beans.property.SimpleStringProperty;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
 import javafx.scene.control.TextArea;
-import javafx.scene.control.TextField;
 import org.aplatanao.billing.rest.client.api.DefaultApi;
+import org.aplatanao.billing.rest.client.model.Invoice;
+import org.aplatanao.billing.rest.client.model.Invoices;
 
 public class Controller {
 
     @FXML
-    private TextField url;
+    private TextArea taskResponse;
 
     @FXML
-    private TextArea response;
+    private TableView<Invoice> invoicesTable;
+
+    @FXML
+    private TableColumn<Invoice, String> codeCol;
+
+    @FXML
+    private TableColumn<Invoice, String> dateCol;
+
+    @FXML
+    private TableColumn<Invoice, String> commentCol;
+
+
+    private ObservableList<Invoice> invoices = FXCollections.observableArrayList();
+
+    @FXML
+    private void initialize() {
+
+        codeCol.setCellValueFactory(p -> new SimpleStringProperty(p.getValue().getCode()));
+        dateCol.setCellValueFactory(p -> new SimpleStringProperty(p.getValue().getDate().toString()));
+        commentCol.setCellValueFactory(p -> new SimpleStringProperty(p.getValue().getComment()));
+
+        invoicesTable.setItems(invoices);
+    }
 
     @FXML
     private void connect(ActionEvent event) {
-        response.setText(new DefaultApi().getTasks().toString());
-}
+        DefaultApi api = new DefaultApi();
+        taskResponse.setText(api.getTasks().toString());
+
+        invoices.clear();
+        int page = 0, size = 20, year = 2018;
+        Invoices data = api.getInvoices(page, size, year);
+
+        this.invoices.addAll(data);
+    }
 }
